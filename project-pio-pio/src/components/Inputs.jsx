6df34331021f3074132f7dd5cd5225950e9fe2c9
Mani Ansari <maniansari12@gmail.com>
2024-08-10 11:41:07 -0700

    search_location

--- project-pio-pio/src/components/Inputs.jsx
+++ project-pio-pio/src/components/Inputs.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
+import { UilSearch, UilCelsius,UilLocationPoint,UilFahrenheit } from '@iconscout/react-unicons'
 
 
 function Inputs() {
   return (
     <div className="flex flex-row justify-center my-6">
-        <div></div>
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+     <input placeholder="e.g Shanghai"type="text" className="text-xl font-light p-2 w-full shadow-xl space-x-4 focus:outline-none capitalize"></input>   
+     <UilSearch size={25} className="text-white cursor-pointer hover:scale-125" />
+     <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"  />
+     </div>
+     <div className="flex flex-row w-1/4 items-center justify-center">
+      <UilCelsius size={25} className="text-white cursor-pointer trasnsition ease-out hover:scale-125"></UilCelsius>
+      <p className="text-white space-x-4 p-2 shadow-xl justify-around mx-1">|</p>
+      <UilFahrenheit size={25} className="text-white cursor-pointer trasnsition ease-out hover:scale-125"></UilFahrenheit>
+     </div>
     </div>
   );
 }
 
-export default Inputs
+export default Inputs;
---
+++ project-pio-pio/src/components/TimeAndLocation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function TimeAndLocation(){
+    return(<div>
+    <div className=" flex items-center justify-center my-6">
+    <p className="text-white  text-xl font-extralight">
+        Friday, 10 August 2024 | Local time: 12:46 PM
+        </p>
+        </div>
+        <div className=" flex items-center justify-center my-3">
+    <p className="text-white  text-3xl font-medium">
+            London, GB
+        </p>
+        </div>
+        </div>
+    );
+}
+export default TimeAndLocation;
